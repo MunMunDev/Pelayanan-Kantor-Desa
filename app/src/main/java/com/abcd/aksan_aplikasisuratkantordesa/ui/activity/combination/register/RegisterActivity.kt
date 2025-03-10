5a2b0c6d44e097ad4bfa22876d625f632632ea0f
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-03-10 11:42:55 +0800

    Commit login and request first commit

---
+++ app/src/main/java/com/abcd/aksan_aplikasisuratkantordesa/ui/activity/combination/register/RegisterActivity.kt
@@ -0,0 +1,15 @@
+package com.abcd.aksan_aplikasisuratkantordesa.ui.activity.combination.register
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.abcd.aksan_aplikasisuratkantordesa.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
